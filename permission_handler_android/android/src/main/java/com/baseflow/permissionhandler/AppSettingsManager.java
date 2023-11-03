328ffb5c8584c2eeccc276d5cfa61c5a7787e5e2
hannvyg <135088871+hannvyg@users.noreply.github.com>
2023-11-03 15:40:57 +0700

    Update AppSettingsManager.java

--- permission_handler_android/android/src/main/java/com/baseflow/permissionhandler/AppSettingsManager.java
+++ permission_handler_android/android/src/main/java/com/baseflow/permissionhandler/AppSettingsManager.java
@@ -23,7 +23,7 @@ final class AppSettingsManager {
         try {
             Intent settingsIntent = new Intent();
             settingsIntent.setAction(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-            settingsIntent.addCategory(Intent.CATEGORY_DEFAULT);
+            // settingsIntent.addCategory(Intent.CATEGORY_DEFAULT);
             settingsIntent.setData(android.net.Uri.parse("package:" + context.getPackageName()));
             settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             // settingsIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
